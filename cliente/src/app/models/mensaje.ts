68ee6cacb382e94015f08c996343a46a5ed84830
NotyCode <josti-olaya@upc.edu.co>
2023-11-24 08:28:09 -0500

    Revert "Inicial"
    This reverts commit 6c2511c3581edeec60726c2b876d448cc1718a00.

--- cliente/src/app/models/mensaje.ts
+++
@@ -1,4 +0,0 @@
-export interface Mensaje {
-  texto: string;
-  tipo: number;
-}
--- cliente/src/app/models/usuario.ts
+++
@@ -1,9 +0,0 @@
-import { Rol } from './rol';
-
-export interface Usuario {
-  id: number;
-  id_rol: number;
-  rol: Rol;
-  nombre: string;
-  activo: string;
-}
